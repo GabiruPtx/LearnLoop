e84fef49f1c61f0b2e9bf764c1edd44f021e7b9b
Garcia <gabrielgarciagc175@gmail.com>
2025-05-25 02:59:19 -0300

    Pré-make da tela principal

--- learnloop/public/urls.py
+++ learnloop/public/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cadastro, name='cadastro'),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
