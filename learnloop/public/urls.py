9615899097769e2130dc12f8d43443c0ebf680ee
Garcia <gabrielgarciagc175@gmail.com>
2025-05-31 15:00:29 -0300

    Correção de urls após rebase

--- learnloop/public/urls.py
+++ learnloop/public/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'public'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('index/', views.index, name='index'),
 ]
